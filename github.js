a62f67211f7579aa4bdfd4ccf14380d5ced411a0
RanaWaqarAhmad <55029599+ranawaqarahmad@users.noreply.github.com>
2020-12-07 22:52:08 +0500

    Add files via upload

---
+++ github.js
@@ -0,0 +1,27 @@
+class GitHub {
+  constructor() {
+    this.client_id = 'ca1d0fe274dd78a49ccd';
+    this.client_secret = 'cd4c170f467ec45662c608510d451b6f10d6aff6';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?
+      per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
